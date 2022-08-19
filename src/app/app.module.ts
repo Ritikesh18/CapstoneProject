720e219cc46f1ab6b0b47d8479eb6f891f5ba147
Ritikesh18 <sirritikesh@gmail.com>
2022-08-19 11:50:20 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+
+//searching catgory wise
+import {Routes,RouterModule} from '@angular/router';
+import { ProductCategoryManuComponent } from './components/product-category-manu/product-category-manu.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+  
+
+  {path:'search/:keyword',component:ProductListComponent},
+  {path:'category/:id', component:ProductListComponent},
+  {path:'category', component:ProductListComponent},
+  {path:'products', component:ProductListComponent},
+  {path: '', redirectTo:'/products',pathMatch:'full'},
+  {path:'**', redirectTo: '/products',pathMatch:'full'},
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryManuComponent,
+    SearchComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
